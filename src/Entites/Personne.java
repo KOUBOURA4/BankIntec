4990cbf1479214e26d310f097268e2a56bcf104d
kadidiatou coulibaly <kadidiatouCoulibaly@gmail.com>
2026-05-02 11:49:41 +0200

    ajoute des objets

--- src/Entites/Personne.java
+++ src/Entites/Personne.java
@@ -12,6 +12,7 @@ public class Personne {
     private Date dateNaissance;
     private Date createdDate;
     private Date updatedDate;
+    private String sex;
 
     public Personne(int id, String nom, String prenom, int age, String adress, String phone, Date dateNaissance, Date createdDate, Date updatedDate) {
         Id = id;
@@ -23,5 +24,101 @@ public class Personne {
         this.dateNaissance = dateNaissance;
         this.createdDate = createdDate;
         this.updatedDate = updatedDate;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Personne{" +
+                "Id=" + Id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", age=" + age +
+                ", adress='" + adress + '\'' +
+                ", phone='" + phone + '\'' +
+                ", dateNaissance=" + dateNaissance +
+                ", createdDate=" + createdDate +
+                ", updatedDate=" + updatedDate +
+                ", sex='" + sex + '\'' +
+                '}';
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import Entites.*;
+
+import java.util.Date;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -5,11 +9,57 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        //Declarations des Objet
+        Compte l1 = new Compte(12,2395,3,new Date(),new Date());
+        Compte l2 = new Compte(19,233495,5,new Date(),new Date());
+        Compte l3 = new Compte(62,2395,9,new Date(),new Date());
+        Compte l4 = new Compte(92,567,8,new Date(),new Date());
+        Compte l5 = new Compte(12,2395,4,new Date(),new Date());
+        System.out.println(l1.toString());
+        System.out.println(l2.toString());
+        System.out.println(l3.toString());
+        System.out.println(l4.toString());
+        System.out.println(l5.toString());
+        CompteEpargne lE1 = new CompteEpargne(12,2337,3,new Date(),new Date());
+        CompteEpargne lE2 = new CompteEpargne(19,233775,4,new Date(),new Date());
+        CompteEpargne lE3 = new CompteEpargne(16,23377,7,new Date(),new Date());
+        CompteEpargne lE4 = new CompteEpargne(197,237837,4,new Date(),new Date());
+        CompteEpargne lE5 = new CompteEpargne(156,98337,38,new Date(),new Date());
+        System.out.println(lE1.toString());
+        System.out.println(lE2.toString());
+        System.out.println(lE3.toString());
+        System.out.println(lE4.toString());
+        System.out.println(lE5.toString());
+        CompteSimple lS1 = new CompteSimple(12,2337,3,new Date(),new Date());
+        CompteSimple lS2 = new CompteSimple(14,2337,6,new Date(),new Date());
+        CompteSimple lS3 = new CompteSimple(12,2337,1,new Date(),new Date());
+        CompteSimple lS4 = new CompteSimple(12,2337,5,new Date(),new Date());
+        CompteSimple lS5 = new CompteSimple(13,2334,4,new Date(),new Date());
+        System.out.println(lS1.toString());
+        System.out.println(lS2.toString());
+        System.out.println(lS3.toString());
+        System.out.println(lS4.toString());
+        System.out.println(lS5.toString());
+        ComptePayant Cp1 = new ComptePayant(23,12345,5,new Date(),new Date());
+        ComptePayant Cp2= new ComptePayant(12,12345,6,new Date(),new Date());
+        ComptePayant Cp3 = new ComptePayant(43,12345,56,new Date(),new Date());
+        ComptePayant Cp4= new ComptePayant(21,12345,45,new Date(),new Date());
+        ComptePayant Cp5 = new ComptePayant(12,12345,233,new Date(),new Date());
+        System.out.println(Cp1.toString());
+        System.out.println(Cp2.toString());
+        System.out.println(Cp3.toString());
+        System.out.println(Cp4.toString());
+        System.out.println(Cp5.toString());
+        Personne p1= new Personne(3,"LYLY","COULIBALY",11,"SDH","19-23-08-34",new Date(),new Date(),new Date());
+        Personne p2= new Personne(76,"Djouma","Marega",10,"SKOL","76-86-45-46",new Date(),new Date(),new Date());
+        Personne p3= new Personne(36,"KADI","DIAWRRA",02,"SYBG","54-67-23-54",new Date(),new Date(),new Date());
+        Personne p4= new Personne(45,"ALI","BAH",11,"UHDH","65-57-89-09",new Date(),new Date(),new Date());
+        Personne p5= new Personne(7,"DILY","LY",12,"ER","13-35-76-45",new Date(),new Date(),new Date());
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
+        System.out.println(p5.toString());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
